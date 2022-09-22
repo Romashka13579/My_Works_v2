fa2ad4a09f2b9f6024c0dce60aaf914821ab6718
Romashka <rroma13579@gmail.com>
2022-09-22 15:11:24 +0000

    tenth commit

--- js/script.js
+++ js/script.js
@@ -2,36 +2,18 @@ var LinksBtn = document.querySelector('.links-profiles-btn');
 var LinksProfiles = document.querySelectorAll('.link-profile');
 
 var i = 0;
+var LinkProfile_old;
 
 LinksBtn.addEventListener('focus', () => {
     LinksProfiles = document.querySelectorAll('.link-profile');
-    // var LinksProfiles_first = LinksProfiles[0];
-    // LinksProfiles_first.style.display = "none"
-    // LinksProfiles.forEach(LinkProfile => {
-    //     LinkProfile.transitione
-    // });
     for(var i = 0; i <= LinksProfiles.length-1; i++){
         LinksProfiles[i].style.display = "block";
-        var LinkProfile_old = LinksProfiles[0];
+        LinkProfile_old = LinksProfiles[i];
+        console.log(LinkProfile_old);
         setTimeout(() => {
-            LinksProfiles.forEach(LinkProfile => {
-                LinkProfile.style.opacity = 1;
-                LinkProfile.addEventListener('transitionend', () => {
-                    console.log("b");
-                    if((LinkProfile_old.style.opacity == 1) || (LinkProfile_old == LinkProfile)){
-                        console.log("c");
-                        LinkProfile_old = LinkProfile;
-                    }
-                });
-            });
-            // console.log("a");
-            // LinksProfiles[i].style.opacity = "1";
-            // LinksProfiles[i].addEventListener('transitionend', () => {
-            //     console.log("b");
-                // if(LinksProfiles[i].style.opacity == 1){
-                //     console.log("c");
-                // }
-            // });
+            LinkProfile_old.style.opacity = 1;
+            console.log("a");
         }, 10);
+        console.log(LinkProfile_old);
     }
 });

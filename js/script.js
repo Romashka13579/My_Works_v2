85e600da501906583c4a933afdca99636cacd8ed
Romashka <rroma13579@gmail.com>
2022-09-24 17:09:35 +0200

    twenty second commit

--- js/script.js
+++ js/script.js
@@ -68,14 +68,16 @@ headerBotBtn.addEventListener('click', () => {
         faArrowUp.style.transform = "matrix(1.00,0.00,0.00,1.00,0,0)";
         headerBottomOverflow.style.display = "block";
         setTimeout(() => {
-            main.style.paddingTop = "200px"
+            main.style.transition = "padding 750ms";
+            main.style.paddingTop = "192px"
             headerBottomOverflow.style.height = "80px";
             headerBottom.style.top = "0px";
         }, 10);
     }
     else if(headerclicked == 0){
         headerclicked = 1;
-        main.style.paddingTop = "120px"
+        main.style.transition = "padding 750ms";
+        main.style.paddingTop = "110px"
         faArrowUp.style.transform = "matrix(-1.00,0.00,-0.00,-1.00,0,0)";
         headerBottom.style.top = "-80px";
         headerBottomOverflow.style.height = "0px";

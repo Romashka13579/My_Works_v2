e4eab6bc55c3231a600c5a289e67cfbb2e5617aa
Romashka <rroma13579@gmail.com>
2022-10-05 18:28:17 +0200

    seventy third commit

--- js/s_comments.js
+++ js/s_comments.js
@@ -14,8 +14,29 @@ mainCommentsTextbarBtn.addEventListener('click', () => {
         commentsBlock.append(clone);
         mainCommentsTextbar.value = "";
         var commentsBlocks1 = document.querySelectorAll('.comment-block-1');
-        // CommentsArray(commentsBlocks1);
+        CommentsArray(commentsBlocks1);
     }
 });
 
-// function CommentsArray(array)
+function CommentsArray(array){
+    array.forEach(arrayelement => {
+        arrayelement.querySelector('.fa-square-minus').addEventListener('click', () => {
+            arrayelement.remove();
+        });
+        arrayelement.querySelector('.fa-pen').addEventListener('click', () => {
+            arrayelement.querySelector('.comment-block-txt').style.display = "none";
+            if(arrayelement.querySelector('.comment-block-txt').innerHTML != ""){
+                arrayelement.querySelector('.comment-block-txt-in-change').value = arrayelement.querySelector('.comment-block-txt').innerHTML;
+                arrayelement.querySelector('.comment-block-txt').innerHTML = "";
+            }
+            arrayelement.querySelector('.comment-block-txt-in-change').style.display = "block";
+            arrayelement.querySelector('.comment-block-txt-in-change').addEventListener('keypress', (e) => {
+                if(e.key === 'Enter'){
+                    arrayelement.querySelector('.comment-block-txt').innerHTML = arrayelement.querySelector('.comment-block-txt-in-change').value;
+                    arrayelement.querySelector('.comment-block-txt').style.display = "block";
+                    arrayelement.querySelector('.comment-block-txt-in-change').style.display = "none";
+                }
+            });
+        });
+    });
+}
